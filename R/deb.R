f648756cf78552b96c4701dea96e962ff320c54f
Kirstin Holsman <kholsman@Kirstins-MacBook-Pro-2.local>
2018-03-08 09:17:38 -0800

    additions

--- R/deb.R
+++ R/deb.R
@@ -3,15 +3,28 @@
 #' Rcode developed by: Kirstin Holsman  
 #' kirstin.holsman@noaa.gov
 #' This function calculates the dynamic energy budget as outlined by B. Einarsson et al. / Journal of Theoretical Biology 281 (2011) 1–8
+#' According to DEB theory, each individual allocates a fixed
+#' fraction k of utilized energy from reserves to growth and somatic
+#' maintenance. The rest, (1-k), is then allocated to maturity maintenance and reproduction. 
 #' @param par is a list of parameters used for the model
 #' @param input is a list with inputs for the model including a daily observation of weight, temperature, prey energy density, and predator energy density
-#' @param Em  maximum energy density (J cm-3)
-#' @param Lm  is the max volumetric length (cm)
-#' @param v  is the conductance (cm d-1)
-#' @param f  denotes the functional food response (dimensionless)
-#' @param g (dimensionless) is the energy investment ratio
-#' @param L is the volumetric length
-#' s is the shape coeff that related volumetric length to actual length often obtained by fitting a weight–length relationship of the type M=(s*length)^3 to
+#' @param k  Fraction of energy to somatic growth and maintenance
+#' @param v  Energy conductance (cm d-1), temperature dependent
+#' @param JEAm  Maximum assimilation rate per surface area (mmol d-1 cm-2)
+#' @paran yVE   Yield of structure from reserve in growth, yVE (dimensionless)
+#' @param kj  Fraction of maturity maintenancec (d-1), temperature dependent
+#' @param EpH Maturity energy at puberty (kJ)
+#' @param gamma Growth rate of roe (d-1)
+#' @param TA Arrhenius temperature (K)
+#' @param Tr Reference temperature (K)
+#' @param Mv Volume specific structural mass (mmol cm-3)
+#' @param muE  Chemical potential (J mmol-3)
+#' @param s Shape coefficient that relates volumetric length to actual length; often obtained by fitting a weight–length relationship of the type M=(s*length)^3 
+#' @param Lm Maximum structural (volumetric) length (cm)
+#' @param dv Density of structural volume (g cm-3)
+#' @param pE Energy reserve density (kJ g-1)
+#' @param pR Energy density of roe (kJ g-1)
+#'
 #' @keywords Temperature, scaling, consumption
 #' @export bioE
 #' @examples
@@ -19,68 +32,126 @@
 #'                     RA=0.0075,RB=-0.251, QR=2.6,Tro=13,Trm=18,SA=0.125, Am=2,FA=0.15,UA=0.11)
 #' ebs_data<-list(W=100,TempC=0:10,Eprey=5539.6,Epred=4184,indgst=0,diet=0,fTCmodel=NA,fTrmodel=NA,velmodel=NA)
 #' bioE(data=ebs_data,par=plk_par)
-#' fTfun()
-#'13.56 J mg-1 Brett & Groves 1979 Qox for Fish 
-par<-data.frame(
-  k=.4,
-  v=0.2,
-  JEAm=.23,
-  yVE=0.8,
-  kj=0.001,
-  EpH=3.93,
-  gamma=0.20,
-  TA=9100,
-  Tr=6.5+273,
-  Mv=4.4,
-  mu=500,
-  s=0.161,
-  Lm=2.82,
-  dv=1,
-  pE=39.3,
-  pR=10.00-8.33
-)
-deb<-function(par,input=list(l,TempC,e,L){
-  # data
-  W<-data$W
-  # TempC<-data$TempC
+#' fTfun() 
+par<-c(
+        k=.4,           # Fraction of energy to somatic growth and maintenance
+        v=0.02,         # Energy conductance (cm d-1), temperature dependent
+        JEAm=.23,       # Maximum assimilation rate per surface area (mmol d-1 cm-2)
+        yVE=0.8,        # Yield of structure from reserve growth 
+        kj=0.001,       # Fraction of maturity maintenance (d-1), temperature dependent 
+        EpH=3.93,       # Maturity energy at puberty (kJ)
+        gamma=0.20,     # Growth rate of roe (d-1)
+        TA=9100,        # Arrhenius temperature (K)
+        Tr=6.5+273.15,  # Reference temperature (K)
+        Mv=4.4,         # Volume specific structural mass (mmol cm-3)
+        muE=500,        # Chemical potential (J mmol-3)
+        s=0.161,        # Shape coefficient that relates volumetric length to actual length
+        Lm=2.82,        # Maximum structural (volumetric) length (cm)
+        dv=1,           # Density of structural volume (g cm-3)
+        pE=39.3,        # Energy reserve density (kJ g-1)
+        pR=10.00-8.33   # Energy density of roe (kJ g-1)
+      )
+# Initial values:
+V0    <- 1e-6
+state <- c(E = 0.000124/V0, # reserve density, J/cm^3
+           V = V0)          # structural volume, cm^3
 
-  ### PARAMETERS
-    k<-par$k
-    v<-par$v
-    JEAm<-par$JEAm
-    yVE<-par$yVE
-    kj<-par$kj
-    EpH<-par$EpH
-    gamma<-par$gamma
-    TA<-par$TA
-    Tr<-par$Tr
-    Mv<-par$Mv
-    mu<-par$mu
-    s<-par$s
-    Lm<-par$Lm
-    dv<-par$dv
-    pE<-par$pE
-    pR<-par$pR
+deb<-function(par,input=list(l,TempC,e,W,L=NA)){
+  with(as.list(c(state, par,input)), {
+  # W<-input$W
+  # if(is.na(input$L)) L<-W^(1/3)  # structural volume
+  # 
+  # # data
+  # W<-data$W
+  # TempC<-input$TempC
+  # TempK<-TempC+273.15
+  # # TempC<-data$TempC
+  # 
+  # ### PARAMETERS
+  #   k<-par$k
+  #   v<-par$v
+  #   JEAm<-par$JEAm
+  #   yVE<-par$yVE
+  #   kj<-par$kj
+  #   EpH<-par$EpH
+  #   gamma<-par$gamma
+  #   TA<-par$TA
+  #   Tr<-par$Tr
+  #   Mv<-par$Mv
+  #   muE<-par$mu
+  #   s<-par$s
+  #   Lm<-par$Lm
+  #   dv<-par$dv
+  #   pE<-par$pE
+  #   pR<-par$pR
   
-  
-    Em<-(UE*JEAm)/v
-    g=(v*Mv)/(k*JEAm*yVE)
-    # JEAm maximum assimilation rate per surface area, JEAm (mmol d-1 cm-2)
-    # yVE yield of structure from reserve in growth, yVE (dimensionless)
+    #cals
+    
+    Em<-(muE*JEAm)/v                       # Max ED
+    g=(v*Mv)/(k*JEAm*yVE)                  # energy investment ratio.
+    Eg<-(muE*Mv)/yVE                       # volume specific cost of structural volume, J/cm^3
+    pM<-(k*muE*JEAm)/Lm                    # volume specific somatic maintenance costs, J/(cm^3*d)
     
     
-  # Equation 1: 
-  E<-Em*Lm^3*e*l^3
+    # structural volume V is the amount of biomass
+    # dynamics are such that maintenance is assumed to take precedence over growth. 
+    # L=V^(1/3) denote the structural (volu- metric) length of an individual,
+    
+    # maximum energy density is a bilogical constant characteristic to each spp.
+    # for capelin Em = 586670.43 (J g-11) so muE and JEAm and v were selected to match this value
+    
+    f <- X/(X_K + X)                       # scaled functional response (food limitation)
+    
+    tfun<-PTr*exp((TA/Tr)-(TA/TempK))    # temperature function
+    
+    # rates
+    dEdt <- (p_Am*f - ec*E) / V^(1/3)*tfun                 # reserve density
+    dVdt <- (kap*ec*E*V^(2/3) - p_M*V)/(kap*E + E_G)*tfun  # structural volume
+    
+       
+  #from Kooijman et al. (2008) 
+  # Eq 1: Reserve energy is the energy available to the individual. 
+    # Its source is food uptake and it is the energy an organism 
+    # utilizes for growth and somatic maintenance on one hand, 
+    # and maturity, reproduction and maturity maintenance on 
+    # the other hand. 
+  
+    E<-Em*Lm^3*e*l^3                    # Reserve Energy
+  
+    e = mE/mEm <-Mev/(L^3*JEAm)          # scaled reserve density
+    
+    mE<-(ME*L^3)/Mv                      # reserve density
+    
+    # output variable
+    length <- V^(1/3)/del_M                                # physical length, cm
+  
   # Equation 2:
-  V=(Lm*l)^3
-  # Equation 3:
-  EH=Em*Lm^3*UH
-  # Equation 4:
-  ER=Em*Lm^3*UR
-  # Equation 5-7:
-  dedt<-(v/Lm*l)*(f-e)
+    V=(Lm*l)^3
+  
+  # Equation 3:EH denote a maturity energy. It is important 
+    # vto note that this variable is abstract and does not 
+    # contribute directly to the weight of the fish. Initially, 
+    # energy is allocated to this variable, and the maturity 
+    # maintenance will be a fraction of this energy, kJEH.
+    # When EH exceeds a certain threshold, EHp , the fish is mature and
+    # allocation of energy to EH ceases
+  
+    EH=Em*Lm^3*UH                  # maturity energy
+  
+  # Equation 4: After puberty has been reached, the energy starts to flow to ER,
+    # which is the total energy available for reproduction. 
+    # We note that the dynamics of the energy flow to maturity is 
+    # the same as that to reproduction. This energy will, in turn, be converted into roe.
+  
+    ER=Em*Lm^3*UR
+  
+  # Equation 5-7:f (dimension- less) denotes the functional food response, 
+      # g (dimensionless) is the energy investment ratio.
+  dedt<-(v/Lm*l)*(f-e)              # total energy available for reproduction
+  
   if(l<e) dldt<-(v/(3*Lm))*((e-1)/(e+g))
   if(l>=e) dldt<-0
+  
   # Equation 8:
   if(UH<UpH){
     dURdt<-0
@@ -88,12 +159,16 @@ deb<-function(par,input=list(l,TempC,e,L){
     dURdt<-(v/Lm)*(1-k)*e*l^2*((l+g)/(e+g))*KJ*UpH
   }
   
-  #Roe maturity 
+  
+  #Roe maturity
+  er<-Er/(Em*Lm^3) 
+  
   derdt<-gamma*(UR-er)*er
   #Arrhenius temperature
-  pT<-PTr*exp((TA/Tr)-(TA/TempC))
+
   #Roe and fat
   
+  # Er as the energy converted from the reproduction energy to eggs. 
   Wfat<-((E+ER-Er)/pE)
   Wroe<-(Er/pr)
   M<-dv*V+Wfat+Wroe
@@ -101,92 +176,10 @@ deb<-function(par,input=list(l,TempC,e,L){
   R<-100*(Wroe/M)
   #percentage of the fat weight of the total body weight
   F<-100*(Wfat/M)
-  
+
   # shape coeff that links volumetric length to actual length
   length<-(1/s)*L
   
-  
-  # Waste
-  wdat<-data;wdat$C_in<-C_jgd
-  Waste<-Waste_fun(par=par,data=wdat)
-  F_jgd<-Waste$F 		#eggestion g prey/g fish/d
-  U_jgd<-Waste$U		#excretion g prey/g fish/d	
-  # specific dynamic action j/g fish/ d
-  SDA_jgd<-SA*(C_jgd-F_jgd) 	
-  # Metabolism
-  r_data<-data;r_data$fitLL<-FALSE
-  Resp<-Resp_fun(par=par,data=r_data)
-  fTr<-Resp$fTr		# Temperature function of resp	
-  Act<-Resp$Act  # Activity
-  Vel<-Resp$Vel
-  Rmax_gO2_g_d<-RA*W^RB 		# max Resp in g O2/g fish /d
-  R_gO2_gd<-Rmax_gO2_g_d*fTr # max Resp in g O2/g fish /d
-  R_jgd<-R_gO2_gd*(Qox)*Act  		# Resp in j per g fish / d
-  
-  # Growth	
-  G_jgd<-(C_jgd-(R_jgd+SDA_jgd+F_jgd+U_jgd))  # Growth J fish /g fish /day
-  G_ggd<-G_jgd/Epred  							# Growth in g fish /g fish /day
-  # add a term to allocate lipids to Epred<-fn()
-  # add a term for the remainder to be G
-  # k as a function of temperature ? rather than constant
-  
-  # return(G)
-  return(list(
-    J_per_gd=data.frame(
-      RFR=RFR,
-      Eprey=Eprey,
-      Epred=Epred,
-      TempC=data$TempC,
-      W=data$W,
-      fTc=fTc,
-      fTr=fTr,
-      Act=Act,
-      Vel=Vel,
-      G_jgd=G_jgd,
-      C_jgd=C_jgd,
-      Cmax_jgd=Cmax_jgd,
-      R_jgd=R_jgd,
-      R_Act_jgd=R_jgd*Act,
-      SDA_jgd=SDA_jgd,
-      F_jgd=F_jgd,
-      U_jgd=U_jgd,
-      def="Joules of prey per gram of pred per day"),
-    gFish_per_gFish_d=data.frame(
-      RFR=RFR,
-      Eprey=Eprey,
-      Epred=Epred,
-      TempC=data$TempC,
-      W=data$W,
-      fTc=fTc,
-      fTr=fTr,
-      Act=Act,
-      Vel=Vel,
-      G_ggd=G_jgd/Epred,
-      C_ggd=C_jgd/Epred,
-      Cmax_ggd=Cmax_jgd/Epred,
-      R_ggd=R_jgd/Epred,
-      SDA_ggd=SDA_jgd/Epred,
-      F_ggd=F_jgd/Epred,
-      U_ggd=U_jgd/Epred,
-      def="gram of pred per gram of pred per day"),
-    gPrey_per_gFish_d=data.frame(
-      RFR=RFR,
-      Eprey=Eprey,
-      Epred=Epred,
-      TempC=data$TempC,
-      W=data$W,
-      fTc=fTc,
-      fTr=fTr,
-      Act=Act,
-      Vel=Vel,
-      G_ggd=G_jgd/Eprey,
-      C_ggd=C_jgd/Eprey,
-      Cmax_ggd=Cmax_jgd/Eprey,
-      R_ggd=R_jgd/Eprey,
-      SDA_ggd=SDA_jgd/Eprey,
-      F_ggd=F_jgd/Eprey,
-      U_ggd=U_jgd/Eprey,
-      def="gram of prey per gram of pred per day"))
-  )
+  }
 }
 
