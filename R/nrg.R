f0ed37fd2c775c89c93989b946ee7ec728d93aeb
Christine Stawitz <cstawitz@uw.edu>
2018-06-15 20:24:50 -0700

    Split out bioE.R into a unique R file for each function

---
+++ R/nrg.R
@@ -0,0 +1,12 @@
+#' NRG: A package for simulating growth using bioenergetics models
+#'
+#' The NRG package contains functions to predict consumption,
+#' respiration, waste, and growth of fish based on bioenergetic
+#' energy balance equations.
+#'
+#' @section NRG functions:
+#' The NRG functions ...
+#'
+#' @docType package
+#' @name NRG
+NULL
