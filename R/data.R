1795137e55525bf34f5e46b7b765418ae89ad240
Christine Stawitz <cstawitz@uw.edu>
2018-06-18 09:35:38 -0700

    Start to document data

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' Halibut data
+#'
+#' A dataset for some halibut data.
+#'
+#' @format A data frame with a lot of stuff
+#' @docType data
+#' @usage data(alldat4B)
+"alldat4B"
+
+#' Halibut data
+#'
+#' A dataset for some halibut data.
+#'
+#' @format A data frame with a lot of stuff
+"alldat4CD"
+
+#' Halibut data
+#'
+#' A dataset for some halibut data.
+#'
+#' @format A data frame with a lot of stuff
+"alldatA3"
+
+#' Halibut data
+#'
+#' A dataset for some halibut data.
+#'
+#' @format A data frame with a lot of stuff
+"Halibut_results"
+
+#' Halibut data
+#'
+#' A dataset for some halibut data.
+#'
+#' @format A data frame with a lot of stuff
+"HalibutC"
